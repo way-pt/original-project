56c8b82d6935d8b670e059d1d7dedd5c883ea8df
dzordich <dzordich@gmail.com>
2019-09-02 14:58:46 -0400

    correctly installed rest framework :

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-
+import rest_framework
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'rest_frameork',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
