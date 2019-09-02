ce9b26092665edb85d7f6f0004fd12e6038f53fa
dzordich <dzordich@gmail.com>
2019-09-01 20:47:57 -0400

    created map field

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
-
+from core.models import Map
 # Register your models here.
+admin.site.register(Map)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.files.storage import FileSystemStorage
 
+
+fsI = FileSystemStorage(location="/media/photos")
+fsT = FileSystemStorage(location="media/dataFiles")
 # Create your models here.
+
+class Map(models.Model):
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    data = models.FileField(upload_to="media/dataFiles", storage=fsT)
+    image = models.ImageField(storage=fsI, upload_to="media/photos", null=True, blank=True)
+    
