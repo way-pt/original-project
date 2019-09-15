0c6ea532b6f27b041a5b46bdcab7f581bd8662cc
dzordich <dzordich@gmail.com>
2019-09-15 13:03:27 -0400

    added string method to map model

--- core/models.py
+++ core/models.py
@@ -14,13 +14,15 @@ class Map(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, to_field='username')
     data = models.FileField(upload_to="dataFiles")
     image = models.ImageField(upload_to="photos", null=True, blank=True)
-    name = models.CharField(max_length=50, help_text="A name for the map.", null=True, blank=True)
+    name = models.CharField(max_length=50, help_text="A name for the map.", null=True)
     date = models.DateField(auto_now_add=True, null=True, blank=True)
 
     # Google Maps marker location
     lat = models.DecimalField(max_digits=5, decimal_places=3, blank=True, null=True)
     lng = models.DecimalField(max_digits=5, decimal_places=3, blank=True, null=True)
 
+    def __str__(self):
+        return f'[{self.user.username}] {self.name}  -  {self.date}  -  {self.pk}'
 
     class Meta:
         ordering = ['-date']
