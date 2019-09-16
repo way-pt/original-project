dc7e54f7bbe3d49239fd48be3dfe878ec35397be
Andy Dearing <andy.dearing@gmail.com>
2019-09-16 10:10:48 -0400

    fixed the settings for new aws account

--- pathfinder/settings.py
+++ pathfinder/settings.py
@@ -134,7 +134,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = 'pathfinder-dz'
+AWS_STORAGE_BUCKET_NAME = 'pathfinderv2'
 AWS_S3_REGION_NAME = 'us-east-2'
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 AWS_DEFAULT_ACL = None
